8c4cdbfbbe700417689b12744e563aa35959ea76
Eilidh Morrice-Ashdown <eilidhma@gmail.com>
2022-01-31 11:43:17 -0800

    finished functions

--- index.js
+++ index.js
@@ -8,55 +8,25 @@ app.use(conversionRouter);
 
 app.listen(3000, ()=>console.log('server running'))
 
-// const http = require('http');
-// const url = require('url');
+const getData = async()=>{
+  try{
+    const { data } = router.get()
+    return data;
+  } catch(e) {
+    console.log(e);   
+  }
+}
 
-// const convertKM = (a)=>{
-//   return Number(a) * 0.62
-// }
-// const convertOz = (a)=>{
-//   return Number(a) * 0.125
-// }
-// const convertFeet = (a)=>{
-//   return Number(a) * 12
-// }
+var amount = 100;
 
-// const app = http.createServer((req,res)=>{
-//   const queries = url.parse(req.url, true).query;
-//   console.log(queries.name); 
-
-//   if(url.parse(req.url).pathname === '/kmtomiles'){
-//       res.end(String(queries.a) + 'km is ' + String(convertKM(queries.a)) + 'miles')
-//   }
-//   if(url.parse(req.url).pathname === '/oztocups'){
-//       res.end(String(queries.a) + 'oz is ' + String(convertOz(queries.a)) + 'cups')
-//   }
-//   if(url.parse(req.url).pathname === '/feettoinches'){
-//       res.end(String(queries.a) + 'feet is ' + String(convertFeet(queries.a)) + 'inches')
-//   }
-// });
-
-
-// const getData = async()=>{
-//   try{
-//     const { data } = router.get()
-//     //console.log(data.rates.USD)
-//     return data;
-//   } catch(e) {
-//     console.log(e);   
-//   }
-// }
-
-// var amount = 100;
-
-// const ConvertCurrency = async(data) => {
-//   const curr = await getData()
-//   console.log(amount + "EUR = "+ curr.rates.USD*amount + "USD")
-//   console.log(amount + "EUR = " + curr.rates.AUD*amount + "AUD")
-//   console.log(amount + "EUR = " + curr.rates.CAD*amount + "CAD")
-//   console.log(amount + "EUR = " + curr.rates.PLN*amount + "PLN") 
-//   console.log(amount + "EUR = " + curr.rates.MXN*amount + "MXN")
+const ConvertCurrency = async(data) => {
+  const curr = await getData()
+  console.log(amount + "EUR = "+ curr.rates.USD*amount + "USD")
+  console.log(amount + "EUR = " + curr.rates.AUD*amount + "AUD")
+  console.log(amount + "EUR = " + curr.rates.CAD*amount + "CAD")
+  console.log(amount + "EUR = " + curr.rates.PLN*amount + "PLN") 
+  console.log(amount + "EUR = " + curr.rates.MXN*amount + "MXN")
   
-// }
+}
 
-// ConvertCurrency();
+ConvertCurrency();
